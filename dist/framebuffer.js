76795280d3326239b43ea41dee33eb9cec85e075
David Walker <dwalker@vorteon.com>
2020-05-14 19:45:43 -0400

    Updates

--- dist/framebuffer.js
+++ dist/framebuffer.js
@@ -1,5 +1,4 @@
 "use strict";
-"strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -9,113 +8,162 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-const transitionTimeMs = 5000;
-const transitionPauseTimeMs = 250;
-var framebufferClass;
-(function (framebufferClass) {
-    framebufferClass["active"] = "framebuffer-active";
-    framebufferClass["passive"] = "framebuffer-passive";
-})(framebufferClass || (framebufferClass = {}));
-const buffers = [];
+function initFrameBuffer() {
+    const fb = new frameBuffer();
+    fb.initFrameBuffer();
+}
 if (window === window.parent) {
-    window.onload = rewritePageIntoHostPage;
+    // only modify page if root frame
+    window.onload = initFrameBuffer;
 }
-// only run init if at the root
-// otherwise do nothing
-function rewritePageIntoHostPage() {
-    const style = document.createElement("style");
-    const body = document.createElement("body");
-    const frameBuffer0Parent = document.createElement("div");
-    const frameBuffer1Parent = document.createElement("div");
-    const frameBuffer0 = document.createElement("iframe");
-    const frameBuffer1 = document.createElement("iframe");
-    buffers[0] = frameBuffer0;
-    buffers[1] = frameBuffer1;
-    frameBuffer0Parent.append(frameBuffer0);
-    frameBuffer1Parent.append(frameBuffer1);
-    frameBuffer0.className = "frameBuffer";
-    frameBuffer1.className = "frameBuffer";
-    frameBuffer0.id = "frameBuffer0";
-    frameBuffer1.id = "frameBuffer1";
-    frameBuffer0Parent.id = "frameBuffer0Parent";
-    frameBuffer1Parent.id = "frameBuffer1Parent";
-    body.append(frameBuffer0Parent, frameBuffer1Parent);
-    style.innerHTML = `
-  html,
-  body,
-  .frameBuffer {
-    height: 100vh;
-    width: 100vw;
-    margin: 0;
-    padding: 0;
-    border: 0 none;
-  }
-
-  .frameBuffer {
-    position: absolute;
-    top: 0;
-    left: 0;
-    opacity: 0;
-    overflow: hidden;
-  }
-
-  @keyframes blurFadeOut {
-    from { filter: blur(0px); opacity: 1; z-index: 10; }
-    50% { filter: blur(15px); opacity: 1; z-index: 10; }
-    99% { filter: blur(0px); opacity: 0; z-index: 10; }
-    to { filter: blur(0px); opacity: 0; z-index: 0; }
-  }
-
-  @keyframes blurFadeIn {
-    from { filter: blur(0px); opacity: 0; }
-    50% { filter: blur(15px); opacity: 0; }
-    99% { filter: blur(0px); opacity: 1; }
-    to { filter: blur(0px); opacity: 1; }
-  }
-
-  .${framebufferClass.active} {
-    opacity: 1;
-    z-index: 1;
-    animation-name: blurFadeIn;
-    animation-duration: 1s;
-  }
-
-  .${framebufferClass.passive} {
-    opacity: 0;
-    animation-name: blurFadeOut;
-    animation-duration: 1s;
-  }
-`;
-    // also consider clip-path animations
-    document.body = body;
-    const currentScriptUrl = [
-        ...document.head.getElementsByTagName("script"),
-    ].filter((v) => v.src.endsWith("framebuffer.js"))[0].src;
-    if (!currentScriptUrl) {
-        throw new Error("Expected script to be named framebuffer.js, but unable to find script tag for that name");
+var framebufferCssClass;
+(function (framebufferCssClass) {
+    framebufferCssClass["active"] = "framebuffer-active";
+    framebufferCssClass["passive"] = "framebuffer-passive";
+    framebufferCssClass["initial"] = "framebuffer-initial";
+})(framebufferCssClass || (framebufferCssClass = {}));
+class frameBuffer {
+    constructor() {
+        this.buffers = [
+            document.createElement("iframe"),
+            document.createElement("iframe"),
+        ];
     }
-    // remove everything except the link to this script
-    [...document.head.getElementsByTagName("script")]
-        .filter((v) => v.src !== currentScriptUrl)
-        .forEach((v) => v.remove());
-    [...document.head.childNodes]
-        .filter((v) => v.nodeName.toLowerCase() !== "script")
-        .forEach((v) => v.remove());
-    document.head.append(style);
-    init();
-}
-function init() {
-    window.onpopstate = function (ev) {
-        loadNewBufferFromHistory(ev);
-    };
-    buffers.forEach((buffer) => buffer.addEventListener("load", () => onBufferLoadComplete(buffer)));
-    function onBufferLoadComplete(buffer) {
+    buildFrameBufferParent(buffer, id) {
+        buffer.className = "frameBuffer";
+        buffer.id = "frameBuffer" + id;
+        const frameBufferParent = document.createElement("div");
+        frameBufferParent.append(buffer);
+        return frameBufferParent;
+    }
+    rewritePageIntoHostPage() {
+        const style = document.createElement("style");
+        const body = document.createElement("body");
+        body.append(this.buildFrameBufferParent(this.buffers[0], "0"), this.buildFrameBufferParent(this.buffers[1], "1"));
+        style.innerHTML = `
+      html,
+      body,
+      .frameBuffer {
+        height: 100vh;
+        width: 100vw;
+        margin: 0;
+        padding: 0;
+        border: 0 none;
+      }
+
+      .frameBuffer {
+        position: absolute;
+        top: 0;
+        left: 0;
+        overflow: hidden;
+        animation-duration: 600ms;
+      }
+
+      .${framebufferCssClass.initial} {
+        animation-duration: 0;
+      }
+
+      @keyframes blurFadeOut {
+        from { filter: blur(0px); opacity: 1; z-index: 10; }
+        50% { filter: blur(15px); opacity: 1; z-index: 10; }
+        99% { filter: blur(0px); opacity: 0; z-index: 10; }
+        to { filter: blur(0px); opacity: 0; z-index: 0; }
+      }
+
+      @keyframes blurFadeIn {
+        from { filter: blur(0px); opacity: 0; }
+        50% { filter: blur(15px); opacity: 0; }
+        99% { filter: blur(0px); opacity: 1; }
+        to { filter: blur(0px); opacity: 1; }
+      }
+
+      .${framebufferCssClass.active} {
+        opacity: 1;
+        z-index: 1;
+        animation-name: blurFadeIn;
+      }
+
+      .${framebufferCssClass.passive} {
+        opacity: 0;
+        animation-name: blurFadeOut;
+      }
+    `;
+        // also consider clip-path animations
+        document.body = body;
+        const scriptElements = [...document.head.getElementsByTagName("script")];
+        const frameBufferScriptElement = scriptElements.find((v) => v.src.endsWith("framebuffer.js"));
+        if (!frameBufferScriptElement) {
+            throw new Error("Expected script to be named framebuffer.js, but unable to find script tag for that name");
+        }
+        // remove everything except the link to this script
+        scriptElements
+            .filter((element) => element !== frameBufferScriptElement)
+            .forEach((element) => element.remove());
+        [...document.head.childNodes]
+            .filter((v) => v.nodeName.toLowerCase() !== "script")
+            .forEach((v) => v.remove());
+        document.head.append(style);
+    }
+    initFrameBuffer() {
+        this.rewritePageIntoHostPage();
+        window.onpopstate = (ev) => {
+            this.loadNewBufferFromHistory(ev);
+        };
+        this.buffers.forEach((buffer) => {
+            buffer.addEventListener("load", () => this.onBufferLoadComplete(buffer));
+        });
+        this.loadNewBufferFromUrl(document.URL);
+    }
+    doNewBufferAction(callback) {
+        const newBuffer = this.buffers.find((buffer) => buffer.classList.contains(framebufferCssClass.passive)) || this.buffers[0];
+        callback(newBuffer);
+    }
+    loadNewBufferFromHistory(ev) {
+        this.doNewBufferAction((buffer) => {
+            if (ev.state && ev.state.loadHtmlFromState) {
+                buffer.innerHTML = ev.state.html;
+                this.transition(buffer);
+            }
+        });
+    }
+    transition(bufferToTransitionTo) {
+        const bufferToTransitionFrom = this.buffers.find((v) => v !== bufferToTransitionTo);
+        this.removeClasses(bufferToTransitionTo);
+        this.removeClasses(bufferToTransitionFrom);
+        bufferToTransitionTo.classList.add(framebufferCssClass.active);
+        bufferToTransitionFrom.classList.add(framebufferCssClass.passive);
+    }
+    removeClasses(buffer) {
+        buffer.classList.remove(framebufferCssClass.active, framebufferCssClass.passive);
+    }
+    redirectOnError(buffer) {
+        try {
+            buffer.contentWindow.document;
+            return false;
+        }
+        catch (_a) {
+            // failed to retrieve document, redirecting to same document so
+            // that error is visible to user
+            location.href = buffer.src;
+            return true;
+        }
+    }
+    loadNewBufferFromUrl(path) {
+        this.doNewBufferAction((buffer) => {
+            buffer.src = path;
+        });
+    }
+    onBufferLoadComplete(buffer) {
+        if (this.redirectOnError(buffer)) {
+            return;
+        }
         const framedDocument = buffer.contentWindow.document;
         const origin = framedDocument.location.origin;
         document.title = framedDocument.title;
         if (!framedDocument.documentElement.style.backgroundColor) {
             framedDocument.documentElement.style.backgroundColor = "white";
         }
+        const loadNewBufferFromUrl = (href) => this.loadNewBufferFromUrl(href);
         Array.from(framedDocument.getElementsByTagName("a")).forEach((element) => {
             const href = element.href;
             if (href && href.startsWith(origin)) {
@@ -123,7 +171,7 @@ function init() {
                     return __awaiter(this, void 0, void 0, function* () {
                         e.preventDefault();
                         loadNewBufferFromUrl(href);
-                        console.log("loading" + href);
+                        console.log("loading: " + href);
                         return false;
                     });
                 });
@@ -136,29 +184,6 @@ function init() {
             html: framedDocument.documentElement.outerHTML,
             loadHtmlFromState: true,
         }, framedDocument.title, framedDocument.location.href);
-        transition(buffer);
+        this.transition(buffer);
     }
-    function doNewBufferAction(callback) {
-        const newBuffer = buffers.filter(buffer => buffer.classList.contains(framebufferClass.passive))[0] || buffers[0];
-        callback(newBuffer);
-    }
-    function loadNewBufferFromUrl(path) {
-        doNewBufferAction((v) => (v.src = path));
-    }
-    function loadNewBufferFromHistory(ev) {
-        doNewBufferAction((buffer) => {
-            if (ev.state && ev.state.loadHtmlFromState) {
-                buffer.innerHTML = ev.state.html;
-                transition(buffer);
-            }
-        });
-    }
-    function transition(bufferToTransitionTo) {
-        const bufferToTransitionFrom = buffers.filter((v) => v !== bufferToTransitionTo)[0];
-        bufferToTransitionTo.classList.remove(framebufferClass.passive);
-        bufferToTransitionTo.classList.add(framebufferClass.active);
-        bufferToTransitionFrom.classList.remove(framebufferClass.active);
-        bufferToTransitionFrom.classList.add(framebufferClass.passive);
-    }
-    loadNewBufferFromUrl(document.URL);
 }
--- framebuffer.ts
+++ framebuffer.ts
@@ -1,132 +1,194 @@
-"strict";
-const transitionTimeMs = 5000;
-const transitionPauseTimeMs = 250;
-
-enum framebufferClass {
-  active = "framebuffer-active",
-  passive = "framebuffer-passive",
+function initFrameBuffer() {
+  const fb = new frameBuffer();
+  fb.initFrameBuffer();
 }
-
-const buffers: HTMLIFrameElement[] = [];
 
 if (window === window.parent) {
-  window.onload = rewritePageIntoHostPage;
+  // only modify page if root frame
+  window.onload = initFrameBuffer;
 }
-// only run init if at the root
-// otherwise do nothing
 
-function rewritePageIntoHostPage() {
-  const style = document.createElement("style");
-  const body = document.createElement("body");
+enum framebufferCssClass {
+  active = "framebuffer-active",
+  passive = "framebuffer-passive",
+  initial = "framebuffer-initial",
+}
 
-  const frameBuffer0Parent = document.createElement("div");
-  const frameBuffer1Parent = document.createElement("div");
+class frameBuffer {
+  private buffers: [HTMLIFrameElement, HTMLIFrameElement] = [
+    document.createElement("iframe"),
+    document.createElement("iframe"),
+  ];
 
-  const frameBuffer0 = document.createElement("iframe");
-  const frameBuffer1 = document.createElement("iframe");
+  private buildFrameBufferParent(buffer: HTMLIFrameElement, id: string) {
+    buffer.className = "frameBuffer";
+    buffer.id = "frameBuffer" + id;
 
-  buffers[0] = frameBuffer0;
-  buffers[1] = frameBuffer1;
-
-  frameBuffer0Parent.append(frameBuffer0);
-  frameBuffer1Parent.append(frameBuffer1);
-
-  frameBuffer0.className = "frameBuffer";
-  frameBuffer1.className = "frameBuffer";
-
-  frameBuffer0.id = "frameBuffer0";
-  frameBuffer1.id = "frameBuffer1";
-
-  frameBuffer0Parent.id = "frameBuffer0Parent";
-  frameBuffer1Parent.id = "frameBuffer1Parent";
-
-  body.append(frameBuffer0Parent, frameBuffer1Parent);
-
-  style.innerHTML = `
-  html,
-  body,
-  .frameBuffer {
-    height: 100vh;
-    width: 100vw;
-    margin: 0;
-    padding: 0;
-    border: 0 none;
+    const frameBufferParent = document.createElement("div");
+    frameBufferParent.append(buffer);
+    return frameBufferParent;
   }
 
-  .frameBuffer {
-    position: absolute;
-    top: 0;
-    left: 0;
-    opacity: 0;
-    overflow: hidden;
+  private rewritePageIntoHostPage() {
+    const style = document.createElement("style");
+    const body = document.createElement("body");
+
+    body.append(
+      this.buildFrameBufferParent(this.buffers[0], "0"),
+      this.buildFrameBufferParent(this.buffers[1], "1")
+    );
+
+    style.innerHTML = `
+      html,
+      body,
+      .frameBuffer {
+        height: 100vh;
+        width: 100vw;
+        margin: 0;
+        padding: 0;
+        border: 0 none;
+      }
+
+      .frameBuffer {
+        position: absolute;
+        top: 0;
+        left: 0;
+        overflow: hidden;
+        animation-duration: 600ms;
+      }
+
+      .${framebufferCssClass.initial} {
+        animation-duration: 0;
+      }
+
+      @keyframes blurFadeOut {
+        from { filter: blur(0px); opacity: 1; z-index: 10; }
+        50% { filter: blur(15px); opacity: 1; z-index: 10; }
+        99% { filter: blur(0px); opacity: 0; z-index: 10; }
+        to { filter: blur(0px); opacity: 0; z-index: 0; }
+      }
+
+      @keyframes blurFadeIn {
+        from { filter: blur(0px); opacity: 0; }
+        50% { filter: blur(15px); opacity: 0; }
+        99% { filter: blur(0px); opacity: 1; }
+        to { filter: blur(0px); opacity: 1; }
+      }
+
+      .${framebufferCssClass.active} {
+        opacity: 1;
+        z-index: 1;
+        animation-name: blurFadeIn;
+      }
+
+      .${framebufferCssClass.passive} {
+        opacity: 0;
+        animation-name: blurFadeOut;
+      }
+    `;
+
+    // also consider clip-path animations
+
+    document.body = body;
+
+    const scriptElements = [...document.head.getElementsByTagName("script")];
+
+    const frameBufferScriptElement = scriptElements.find((v) =>
+      v.src.endsWith("framebuffer.js")
+    );
+
+    if (!frameBufferScriptElement) {
+      throw new Error(
+        "Expected script to be named framebuffer.js, but unable to find script tag for that name"
+      );
+    }
+
+    // remove everything except the link to this script
+    scriptElements
+      .filter((element) => element !== frameBufferScriptElement)
+      .forEach((element) => element.remove());
+
+    [...document.head.childNodes]
+      .filter((v) => v.nodeName.toLowerCase() !== "script")
+      .forEach((v) => v.remove());
+
+    document.head.append(style);
   }
 
-  @keyframes blurFadeOut {
-    from { filter: blur(0px); opacity: 1; z-index: 10; }
-    50% { filter: blur(15px); opacity: 1; z-index: 10; }
-    99% { filter: blur(0px); opacity: 0; z-index: 10; }
-    to { filter: blur(0px); opacity: 0; z-index: 0; }
+  public initFrameBuffer() {
+    this.rewritePageIntoHostPage();
+
+    window.onpopstate = (ev: PopStateEvent) => {
+      this.loadNewBufferFromHistory(ev);
+    };
+
+    this.buffers.forEach((buffer) => {
+      buffer.addEventListener("load", () => this.onBufferLoadComplete(buffer));
+    });
+
+    this.loadNewBufferFromUrl(document.URL);
   }
 
-  @keyframes blurFadeIn {
-    from { filter: blur(0px); opacity: 0; }
-    50% { filter: blur(15px); opacity: 0; }
-    99% { filter: blur(0px); opacity: 1; }
-    to { filter: blur(0px); opacity: 1; }
+  private doNewBufferAction(callback: (buffer: HTMLIFrameElement) => void) {
+    const newBuffer =
+      this.buffers.find((buffer) =>
+        buffer.classList.contains(framebufferCssClass.passive)
+      ) || this.buffers[0];
+
+    callback(newBuffer);
   }
 
-  .${framebufferClass.active} {
-    opacity: 1;
-    z-index: 1;
-    animation-name: blurFadeIn;
-    animation-duration: 1s;
+  private loadNewBufferFromHistory(ev: PopStateEvent) {
+    this.doNewBufferAction((buffer) => {
+      if (ev.state && ev.state.loadHtmlFromState) {
+        buffer.innerHTML = ev.state.html;
+        this.transition(buffer);
+      }
+    });
   }
 
-  .${framebufferClass.passive} {
-    opacity: 0;
-    animation-name: blurFadeOut;
-    animation-duration: 1s;
+  private transition(bufferToTransitionTo: HTMLIFrameElement) {
+    const bufferToTransitionFrom = this.buffers.find(
+      (v) => v !== bufferToTransitionTo
+    )!;
+
+    this.removeClasses(bufferToTransitionTo);
+    this.removeClasses(bufferToTransitionFrom);
+
+    bufferToTransitionTo.classList.add(framebufferCssClass.active);
+    bufferToTransitionFrom.classList.add(framebufferCssClass.passive);
   }
-`;
 
-  // also consider clip-path animations
-
-  document.body = body;
-
-  const currentScriptUrl = [
-    ...document.head.getElementsByTagName("script"),
-  ].filter((v) => v.src.endsWith("framebuffer.js"))[0].src;
-
-  if (!currentScriptUrl) {
-    throw new Error(
-      "Expected script to be named framebuffer.js, but unable to find script tag for that name"
+  private removeClasses(buffer: HTMLIFrameElement) {
+    buffer.classList.remove(
+      framebufferCssClass.active,
+      framebufferCssClass.passive
     );
   }
 
-  // remove everything except the link to this script
-  [...document.head.getElementsByTagName("script")]
-    .filter((v) => v.src !== currentScriptUrl)
-    .forEach((v) => v.remove());
+  private redirectOnError(buffer: HTMLIFrameElement): boolean {
+    try {
+      buffer.contentWindow!.document;
+      return false;
+    } catch {
+      // failed to retrieve document, redirecting to same document so
+      // that error is visible to user
+      location.href = buffer.src;
+      return true;
+    }
+  }
 
-  [...document.head.childNodes]
-    .filter((v) => v.nodeName.toLowerCase() !== "script")
-    .forEach((v) => v.remove());
+  private loadNewBufferFromUrl(path: string) {
+    this.doNewBufferAction((buffer) => {
+      buffer.src = path;
+    });
+  }
 
-  document.head.append(style);
+  private onBufferLoadComplete(buffer: HTMLIFrameElement) {
+    if (this.redirectOnError(buffer)) {
+      return;
+    }
 
-  init();
-}
-
-function init() {
-  window.onpopstate = function (this: WindowEventHandlers, ev: PopStateEvent) {
-    loadNewBufferFromHistory(ev);
-  };
-
-  buffers.forEach((buffer) =>
-    buffer.addEventListener("load", () => onBufferLoadComplete(buffer))
-  );
-
-  function onBufferLoadComplete(buffer: HTMLIFrameElement) {
     const framedDocument = buffer.contentWindow!.document;
     const origin = framedDocument.location.origin;
     document.title = framedDocument.title;
@@ -134,6 +196,9 @@ function init() {
     if (!framedDocument.documentElement.style.backgroundColor) {
       framedDocument.documentElement.style.backgroundColor = "white";
     }
+
+    const loadNewBufferFromUrl = (href: string) =>
+      this.loadNewBufferFromUrl(href);
 
     Array.from(framedDocument.getElementsByTagName("a")).forEach((element) => {
       const href = element.href;
@@ -143,7 +208,7 @@ function init() {
           e.preventDefault();
           loadNewBufferFromUrl(href);
 
-          console.log("loading" + href);
+          console.log("loading: " + href);
 
           return false;
         });
@@ -161,42 +226,6 @@ function init() {
       framedDocument.location.href
     );
 
-    transition(buffer);
+    this.transition(buffer);
   }
-
-  function doNewBufferAction(callback: (buffer: HTMLIFrameElement) => void) {
-    const newBuffer =
-      buffers.filter(
-        buffer => buffer.classList.contains(framebufferClass.passive)
-      )[0] || buffers[0];
-
-    callback(newBuffer);
-  }
-
-  function loadNewBufferFromUrl(path: string) {
-    doNewBufferAction((v) => (v.src = path));
-  }
-
-  function loadNewBufferFromHistory(ev: PopStateEvent) {
-    doNewBufferAction((buffer) => {
-      if (ev.state && ev.state.loadHtmlFromState) {
-        buffer.innerHTML = ev.state.html;
-        transition(buffer);
-      }
-    });
-  }
-
-  function transition(bufferToTransitionTo: HTMLIFrameElement) {
-    const bufferToTransitionFrom = buffers.filter(
-      (v) => v !== bufferToTransitionTo
-    )[0];
-
-    bufferToTransitionTo.classList.remove(framebufferClass.passive);
-    bufferToTransitionTo.classList.add(framebufferClass.active);
-
-    bufferToTransitionFrom.classList.remove(framebufferClass.active);
-    bufferToTransitionFrom.classList.add(framebufferClass.passive);
-  }
-
-  loadNewBufferFromUrl(document.URL);
 }
